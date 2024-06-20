70ae1f860bd5c7488cc92fe7ab422fe1cfe139eb
Hari-000 <165980921+Hari-000@users.noreply.github.com>
2024-06-20 00:32:24 -0400

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "the8ucket" # Replace with your actual S3 bucket name
-    key    = "Jenkins-1/terraform.tfstate"
+    key    = "Jenkins-step2/terraform.tfstate"
     region = "us-east-2"
   }
 }
